68b98946ffe01714f55544d1811db1f861563413
Amilie <178850994+AmilieCoding@users.noreply.github.com>
2025-12-30 19:08:23 +0000

    Add settings capabilities.

--- sillicat/module/impl/movement/Flight.java
+++ sillicat/module/impl/movement/Flight.java
@@ -3,37 +3,68 @@ package sillicat.module.impl.movement;
 import sillicat.module.Category;
 import sillicat.module.Module;
 import sillicat.module.ModuleInfo;
+import sillicat.setting.impl.ModeSetting;
+import sillicat.setting.impl.NumberSetting;
 
 @ModuleInfo(
         name = "Flight",
         description = "Accelerate at speed through the air.",
         category = Category.Movement,
-        enabled = true
+        enabled = false
 )
 
 public class Flight extends Module {
+    private final ModeSetting mode = new ModeSetting("Mode", "Creative", "Jetpack", "Glide");
+    private final NumberSetting speed = new NumberSetting("Speed", 1, 0.1, 10, 0.1);
+
+    public Flight(){
+        addSettings(mode, speed);
+    }
+
+    private double oldY;
+    private float oldJ;
 
     @Override
     public void onUpdate() {
         if(mc.thePlayer != null){
-            mc.thePlayer.capabilities.isFlying = true;
+            oldY = mc.thePlayer.motionY;
+            oldJ = mc.thePlayer.jumpMovementFactor;
 
-            if(mc.gameSettings.keyBindJump.isPressed()){
-                mc.thePlayer.motionY += 0.2;
+            if(mode.getCurrMode().equalsIgnoreCase("Creative")){
+                mc.thePlayer.capabilities.isFlying = true;
+
+                if(mc.gameSettings.keyBindJump.isPressed()){
+                    mc.thePlayer.motionY += 0.2;
+                }
+
+                if(mc.gameSettings.keyBindSneak.isPressed()){
+                    mc.thePlayer.motionY -= 0.2;
+                }
+
+                if(mc.gameSettings.keyBindForward.isPressed()){
+                    mc.thePlayer.capabilities.setFlySpeed((float) (speed.getVal() / 20%.1f));
+                }
             }
 
-            if(mc.gameSettings.keyBindSneak.isPressed()){
-                mc.thePlayer.motionY -= 0.2;
+            if(mode.getCurrMode().equalsIgnoreCase("Jetpack")){
+                if(mc.gameSettings.keyBindJump.isPressed()){
+                    mc.thePlayer.jump();
+                }
             }
 
-            if(mc.gameSettings.keyBindForward.isPressed()){
-                mc.thePlayer.capabilities.setFlySpeed(0.25f);
+            if(mode.getCurrMode().equalsIgnoreCase("Glide")){
+                if((mc.thePlayer.motionY < 0.0D) && (mc.thePlayer.isAirBorne) && (!mc.thePlayer.isInWater()) && (!mc.thePlayer.isInLava()) && (!mc.thePlayer.isOnLadder())){
+                    mc.thePlayer.motionY = -.125D;
+                    mc.thePlayer.jumpMovementFactor *= 1.12337f;
+                }
             }
         }
     }
 
     @Override
     public void onDisable() {
+        mc.thePlayer.motionY = oldY;
+        mc.thePlayer.jumpMovementFactor = oldJ;
         mc.thePlayer.capabilities.isFlying = false;
     }
 }
