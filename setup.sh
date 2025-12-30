2e2022ebb434f0b6e9c45f82e2f8efae8e7287cf
Amilie <178850994+AmilieCoding@users.noreply.github.com>
2025-12-30 10:38:22 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,33 +1,57 @@
-echo "Downloading client sources."
+#!/usr/bin/env bash
+set -e
 
-git clone https://github.com/Marcelektro/MavenMCP-1.8.9.git ../
-wget https://github.com/Hexeption/Optifine-SRC/raw/refs/heads/master/Optifine%20SRC%20Version%20%5B1.8.9%20HD%20U%20M6%20pre2%5D.rar ../
+echo "=== Sillicat Dev Setup ==="
 
-echo "Setting up Optifine and basic source."
+# Variables
+MCP_DIR="MavenMCP-1.8.9"
+SILLICAT_DIR="sillicat-dist"
+OPTIFINE_RAR="Optifine_SRC_1.8.9_HD_U_M6_pre2.rar"
+DEV_DIR="sillicat-prepped"
 
-rm -rf ../MavenMCP-1.8.9/src/main/java/net
+# Clean previous runs
+rm -rf "$MCP_DIR" "$SILLICAT_DIR" Optifine-SRC "$DEV_DIR"
+
+# Download sources
+echo "Downloading MCP 1.8.9..."
+git clone https://github.com/Marcelektro/MavenMCP-1.8.9.git "$MCP_DIR"
+
+echo "Downloading Optifine 1.8.9 sources..."
+wget -O "$OPTIFINE_RAR" "https://github.com/Hexeption/Optifine-SRC/raw/refs/heads/master/Optifine%20SRC%20Version%20%5B1.8.9%20HD%20U%20M6%20pre2%5D.rar"
+
+# Extract Optifine
+echo "Extracting Optifine..."
 mkdir Optifine-SRC
-mv 'Optifine SRC Version [1.8.9 HD U M6 pre2].rar' Optifine-SRC
-unrar x ../Optifine-SRC/'Optifine SRC Version [1.8.9 HD U M6 pre2].rar'
-cp -r ../Optifine-SRC/net ../MavenMCP-1.8.9/src/main/java/net
-rm -rf ../MavenMCP-1.8.9/src/main/resources/assets
-cp -r ../Optifine-SRC/assets ../MavenMCP-1.8.9/src/main/resources/assets
+unrar x "$OPTIFINE_RAR" Optifine-SRC/
 
-echo "Setting up sillicat."
+# Setup Optifine sources
+rm -rf "$MCP_DIR/src/main/java/net"
+cp -r Optifine-SRC/net "$MCP_DIR/src/main/java/"
+rm -rf "$MCP_DIR/src/main/resources/assets"
+cp -r Optifine-SRC/assets "$MCP_DIR/src/main/resources/assets"
 
-git clone https://github.com/amiliecoding/sillicat-dist
-cp -r ../sillicat-dist/sillicat ../MavenMCP-1.8.9/src/main/java/
+# Setup Sillicat
+echo "Downloading Sillicat..."
+git clone https://github.com/amiliecoding/sillicat-dist "$SILLICAT_DIR"
+cp -r "$SILLICAT_DIR/sillicat" "$MCP_DIR/src/main/java/"
+rm "$MCP_DIR/pom.xml"
+cp "$SILLICAT_DIR/pom.xml" "$MCP_DIR/pom.xml"
 
-rm ../MavenMCP-1.8.9/pom.xml
-cp ../sillicat-dist/pom.xml ../MavenMCP-1.8.9/pom.xml
+# Apply patches safely
+echo "Applying patches..."
+PATCH_DIR="$(pwd)/$SILLICAT_DIR/sillicat/patches"
 
-echo "Patching sources..."
+for patch_file in "$PATCH_DIR"/*.patch; do
+    echo "Applying $(basename "$patch_file")..."
+    patch -p0 -d "$MCP_DIR/src/main/java" < "$patch_file"
+done
 
-patch -p0 <../sillicat-dist/sillicat/patches/Minecraft.patch
-patch -p0 <../sillicat-dist/sillicat/patches/ModelRotation.patch
-patch -p0 <../sillicat-dist/sillicat/patches/OpenGlHelper.patch
-patch -p0 <../sillicat-dist/sillicat/patches/KeyBinding.patch
-patch -p0 <../sillicat-dist/sillicat/patches/GuiIngame.patch
-patch -p0 <../sillicat-dist/sillicat/patches/EntityPlayerSP.patch
+# Move prepared MCP to dev folder
+mv "$MCP_DIR" "$DEV_DIR"
 
-echo "Patched, and downloaded - please see README.silly, for setting up Intellij."
+# Cleanup temporary files
+echo "Cleaning up temporary files..."
+rm -rf "$SILLICAT_DIR" Optifine-SRC "$OPTIFINE_RAR"
+
+echo "=== Sillicat development environment ready at $DEV_DIR ==="
+echo "Open $DEV_DIR in IntelliJ as a Maven project."
