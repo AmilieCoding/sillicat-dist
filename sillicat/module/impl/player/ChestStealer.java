c0d23c00515dcd093a0eb2cf3d9bc9de3bed6bd7
Amilie <178850994+AmilieCoding@users.noreply.github.com>
2025-12-31 13:52:28 +0000

    Improved stealer.

--- sillicat/module/impl/player/ChestStealer.java
+++ sillicat/module/impl/player/ChestStealer.java
@@ -4,8 +4,13 @@ import net.minecraft.client.gui.inventory.GuiChest;
 import sillicat.module.Category;
 import sillicat.module.Module;
 import sillicat.module.ModuleInfo;
+import sillicat.setting.impl.BooleanSetting;
 import sillicat.setting.impl.ModeSetting;
 import sillicat.setting.impl.NumberSetting;
+
+import net.minecraft.inventory.ContainerChest;
+import net.minecraft.inventory.Slot;
+
 
 @ModuleInfo(
         name = "ChestStealer",
@@ -13,24 +18,69 @@ import sillicat.setting.impl.NumberSetting;
         category = Category.Player,
         enabled = false
 )
-
 public class ChestStealer extends Module {
+
     private final ModeSetting mode = new ModeSetting("Mode", "Steal", "Stash");
     public static final NumberSetting speed = new NumberSetting("Speed (ms)", 100, 5, 100, 5);
+    private final BooleanSetting close = new BooleanSetting("Close", false);
 
-    public ChestStealer(){
-        addSettings(mode, speed);
+    private boolean finished = false;
+    private long lastAction = 0L;
+
+    public ChestStealer() {
+        addSettings(mode, speed, close);
     }
 
     @Override
     public void onUpdate() {
-        if(mc.thePlayer != null && mc.currentScreen instanceof GuiChest){
-            if(mode.getCurrMode().equalsIgnoreCase("Steal")) {
+        if (mc.thePlayer == null || !(mc.currentScreen instanceof GuiChest)) {
+            finished = false;
+            super.onUpdate();
+            return;
+        }
+
+        GuiChest chest = (GuiChest) mc.currentScreen;
+
+        long now = System.currentTimeMillis();
+        if (now - lastAction < (long) speed.getVal()) {
+            super.onUpdate();
+            return;
+        }
+        lastAction = now;
+
+        if (!finished) {
+            if (mode.getCurrMode().equalsIgnoreCase("Steal")) {
                 GuiChest.INSTANCE.steal();
-            }if(mode.getCurrMode().equalsIgnoreCase("Stash")){
+            } else if (mode.getCurrMode().equalsIgnoreCase("Stash")) {
                 GuiChest.INSTANCE.stash();
             }
+
+            if (isChestEmpty(chest)) {
+                finished = true;
+            }
+
+            super.onUpdate();
+            return;
         }
+
+        if (close.isEnabled()) {
+            mc.thePlayer.closeScreen();
+            finished = false;
+        }
+
         super.onUpdate();
+    }
+
+    private boolean isChestEmpty(GuiChest chest) {
+        ContainerChest container = (ContainerChest) chest.inventorySlots;
+        int chestSize = container.getLowerChestInventory().getSizeInventory();
+
+        for (int i = 0; i < chestSize; i++) {
+            Slot slot = container.getSlot(i);
+            if (slot.getHasStack()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
